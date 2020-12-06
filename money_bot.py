00745868e2c7e90adefc360c9e938d5a419fc1b1
Bodyk1Love <vanya.zaliznyak@gmail.com>
2020-12-06 19:11:14 +0200

    db version

--- money_bot.py
+++ money_bot.py
@@ -1,41 +1,71 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import telegram
 from pprint import pprint as pp
+from sqlite3 import Error
 import os
-PORT = int(os.environ.get('PORT', 5000))
+import sqlite3
 
+
+PORT = int(os.environ.get('PORT', 5000))
 bot = telegram.Bot(token=open("secret.cnf").read())
 
 
+def post_sql_query(sql_query):
+    print(sql_query)
+    with sqlite3.connect('db.db') as connection:
+        cursor = connection.cursor()
+        try:
+            cursor.execute(sql_query)
+            connection.commit()
+        except Error:
+            pass
+        result = cursor.fetchall()
+        return result
+
+
+def get_amount(update):
+    data = update.to_dict()['message']['text'].split()
+    try:
+        amount = float(data[1])
+        comment = ""
+        if len(data) > 2:
+            comment = " ".join(data[2:])
+        return [amount, comment]
+    except Exception as error:
+        update.message.reply_text(error)
+
+
 def balance(update, context):
-    with open("balance", "r") as file:
-        update.message.reply_text(file.read())
+    b = post_sql_query("SELECT SUM(amount) FROM logs")
+    update.message.reply_text(b[0][0])
+
+
+def get_minus(update, context):
+    data = post_sql_query(
+        f"SELECT amount, date, comment FROM logs WHERE amount < 0")
+    res = "\n".join([f"{i[1]}) {i[0]} {i[2]}" for i in data])
+    update.message.reply_text(res)
+
+
+def get_plus(update, context):
+    data = post_sql_query(
+        f"SELECT amount, date, comment FROM logs WHERE amount > 0")
+    res = "\n".join([f"{i[1]}) {i[0]} {i[2]}" for i in data])
+    update.message.reply_text(res)
 
 
 def minus(update, context):
-    data = update.to_dict()
-    balance = float(open("balance").read() or 0)
-    try:
-        amount = float(data['message']['text'].split()[1])
-    except:
-        update.message.reply_text("Хуй соси")
-    balance -= amount
-    with open("balance", "w") as file:
-        file.write(str(balance))
-    update.message.reply_text(balance)
+    amount, comment = get_amount(update)
+    post_sql_query(
+        f"INSERT INTO logs (amount, comment) VALUES ('-{abs(amount)}', '{comment}')")
+    update.message.reply_text("Записав")
 
 
 def plus(update, context):
-    data = update.to_dict()
-    balance = float(open("balance").read() or 0)
-    try:
-        amount = float(data['message']['text'].split()[1])
-    except:
-        update.message.reply_text("Хуй соси")
-    balance += amount
-    with open("balance", "w") as file:
-        file.write(str(balance))
-    update.message.reply_text(balance)
+    amount, comment = get_amount(update)
+    post_sql_query(
+        f"INSERT INTO logs (amount, comment) VALUES ({amount}, '{comment}')")
+    update.message.reply_text("Записав")
 
 
 def error(update, context):
@@ -52,6 +82,8 @@ def main():
     dp.add_handler(CommandHandler("balance", balance))
     dp.add_handler(CommandHandler("minus", minus))
     dp.add_handler(CommandHandler("plus", plus))
+    dp.add_handler(CommandHandler("get_minus", get_minus))
+    dp.add_handler(CommandHandler("get_plus", get_plus))
 
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
